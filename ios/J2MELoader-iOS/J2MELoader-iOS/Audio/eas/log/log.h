0a648c15b538dd2e81fc3a3eacdc3f54e7f67a01
PhamTriHien <phamtrihien@users.noreply.github.com>
2026-09-05 02:09:32 +0700

    fix(audio): add Android header stubs for EAS synth on iOS

---
+++ ios/J2MELoader-iOS/J2MELoader-iOS/Audio/eas/log/log.h
@@ -0,0 +1,14 @@
+﻿#ifndef LOG_H
+#define LOG_H
+#define ALOGE(...) ((void)0)
+#define ALOGW(...) ((void)0)
+#define ALOGI(...) ((void)0)
+#define ALOGV(...) ((void)0)
+#define ALOGD(...) ((void)0)
+#define LOGE(...) ((void)0)
+#define LOGW(...) ((void)0)
+#define LOGI(...) ((void)0)
+#define LOGV(...) ((void)0)
+#define LOGD(...) ((void)0)
+#define LOG_FATAL_IF(cond, ...) ((void)0)
+#endif
